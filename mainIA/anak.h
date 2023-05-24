251ac8b01caf46240923d490b46b850af3d3d2a8
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-24 17:12:53 +0700

    Memasukan Class anak

--- mainIA/anak.h
+++ mainIA/anak.h
@@ -1,2 +1,14 @@
 #ifndef ANAK_H
 #define Anak_H
+
+class anak {
+public:
+	string nama;
+	anak(string pNama) :nama(pNama) {
+		cout << "Anak \"" << nama << "\" ada\n";
+	}
+	~anak() {
+		cout << "Anak \"" << nama << "\" tidak ada\n";
+	}
+};
+#endif // !ANAK_H
