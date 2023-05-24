f153d2baa267dded61cd164a08879e8644f9d7dc
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-24 15:22:53 +0700

    memasukan siswa 1

--- ParadigmaOOP_023/ParadigmaOOP_023.cpp
+++ ParadigmaOOP_023/ParadigmaOOP_023.cpp
@@ -36,3 +36,11 @@ public:
 		return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n\n";
 	}
 };
+
+int main() {
+	pelajar siswa1("Andi Laksono", "SMAN 1 Bantul");
+	cout << siswa1.perkenalan();
+	cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+	return 0;
+}
