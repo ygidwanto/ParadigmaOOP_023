3bfe6845be887592ff5ea3bdd4902006ba166e10
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-24 16:24:19 +0700

    menambahkan class dokter dan pasien

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -2,3 +2,18 @@
 #include <vector>
 using namespace std;
 
+class dokter;
+class pasien {
+public:
+	string nama;
+	vector<dokter*> daftar_dokter;
+	pasien(string pNama) :nama(pNama) {
+		cout << "Pasien \"" << nama << "\" ada\n";
+	}
+	~pasien() {
+		cout << "Pasien \"" << nama << "\" tidak ada\n";
+	}
+	void tambahDokter(dokter*);
+	void cetakDokter();
+};
+
