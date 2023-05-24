f776a3594234a7ff5c444d80e413db9f6a0fb7a4
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-24 17:30:55 +0700

    memasukan class jantung dengan cara menghidupkan jantung kemudian mematikan jantung

--- mainJM/manusia.h
+++ mainJM/manusia.h
@@ -1,3 +1,14 @@
 #ifndef MANUSIA_H
 #defline MANUSIA_H
 using namespace std;
+
+class jantung {
+public:
+	jantung() {
+		cout << "jantung dihidupkan\n";
+	}
+	~jantung() {
+		cout << "jantung dimatikan\n";
+	}
+};
+#endif
