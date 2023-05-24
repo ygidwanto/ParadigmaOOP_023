2f8b871487581a674a98a6f412e0feff3b5d83a1
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-24 16:57:42 +0700

    memasukan class ibu

--- mainIA/a. ibu.h
+++ mainIA/a. ibu.h
@@ -1,4 +1,19 @@
 #ifndef IBU_H
 #define IBU_H
 
+class ibu {
+public:
+	string nama;
+	vector<anak*> daftar_anak;
+
+	ibu(string pNama) {
+		cout << "Ibu \"" << nama << "\" ada\n";
+	}
+	~ibu() {
+		cout << "Ibu \"" << nama << "\" tidak ada\n";
+	}
+	void tambahAnak(anak* pAnak);
+	void cetakAnak();
+};
+
 #endif
