1150346efbafa060cf2badfc106064ec8a3f2601
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-24 15:21:19 +0700

    membuat string perkenalan

--- ParadigmaOOP_023/ParadigmaOOP_023.cpp
+++ ParadigmaOOP_023/ParadigmaOOP_023.cpp
@@ -17,4 +17,22 @@ public:
 	int jumlah(int a, int b) {
 		return a + b;
 	}
+
+};
+
+class pelajar : public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pSekolah) :
+		orang(pNama),
+		sekolah(pSekolah) {
+		cout << "pelajar dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << "pelajar dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n\n";
+	}
 };
